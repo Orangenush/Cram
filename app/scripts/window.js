ec6bb1e2296e65994246ff2cd00fbe9ee7730cd8
Ivan <reide740@gmail.com>
2015-04-28 21:20:27 +0300

    [016] Refactoring, chat and check system

--- app/scripts/window.js
+++ app/scripts/window.js
@@ -1,13 +1,3 @@
 var gui = require('nw.gui');
 var win = gui.Window.get();
 win.isMaximized = false;
-
-document.getElementById('windowControlMinimize').onclick = function()
-{
-    win.minimize();
-};
-
-document.getElementById('windowControlClose').onclick = function()
-{
-    win.close();
-};
